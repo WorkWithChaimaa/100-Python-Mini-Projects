ef4cb9fad3a0e232e5bf7485d17946e1de815d52
chaimaa <chamitachomy29@gmail.com>
2025-12-08 12:25:37 -0800

    Exercise 10: Convert Dictionary Keys and Values to Lists

---
+++ 010_dict_to_lists/README.md
@@ -0,0 +1,12 @@
+Exercise 10: Convert Dictionary Keys and Values to Lists
+
+## Objective: 
+Learn how to extract dictionary keys and values as lists.
+
+## Task:
+  * Convert dictionary keys into a list.
+  * Convert dictionary values into a list.
+ 
+## Expected Output:
+- Keys: ['name', 'age', 'language']
+- Values: ['Lkhibra Academy', 5, 'Python']
---
+++ 010_dict_to_lists/main.py
@@ -0,0 +1,14 @@
+# Exercise 10: Convert Dictionary Keys and Values to Lists
+
+# 1. Declare the initial dictionary of keys and values
+dictionary = {"name": "Lkhibra Academy",
+             "age": 5,
+             "Language": "Python"}
+
+# 2. Convert Dictionary Keys into a List
+keys_list = list(dictionary.keys())
+print(f"Keys: {keys_list}")
+
+# 3. Convert Dictionary Values into a List
+values_list = list(dictionary.values())
+print(f"values: {values_list}")
