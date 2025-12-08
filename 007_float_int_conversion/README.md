e990473ec1b81439d76868df700a59779b08dda9
chaimaa <chamitachomy29@gmail.com>
2025-12-08 10:07:50 -0800

    Exercise 7: Convert Float to Integer and Vice Versa

---
+++ 007_float_int_conversion/README.md
@@ -0,0 +1,13 @@
+Exercise 7: Convert Float to Integer and Vice Versa
+
+## Objective
+Learn how to convert between float and integer values.
+
+## Task 
+   * Convert a float 9.75 into an integer.
+   * Convert an integer 50 into a float.
+   * Print both values along with their data types.
+
+## Expected Output
+- Float to Int: 9, Type: <class 'int'>
+- Int to Float: 50.0, Type: <class 'float'
---
+++ 007_float_int_conversion/main.py
@@ -0,0 +1,13 @@
+# Exercise 7: Convert Float to Integer and Vice Versa
+
+# ---Part1: Convert Float to Integer---
+initial_float = 9.75
+result_int = int(initial_float)
+# Print the integer and it's type
+print(f"Float to Int: {result_int}, Type: {type(result_int)}")
+
+# ---Part2: Convert integer to Float---
+initial_int = 50
+result_float = float(initial_int)
+# Print the Float and it's type
+print(f"Int to Float: {result_float}, Type: {type(result_float)}")
