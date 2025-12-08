8924aa65ea6a3477d0acfba061f9c479bba07d47
chaimaa <chamitachomy29@gmail.com>
2025-12-08 11:51:07 -0800

    Exercise 8: Convert a Boolean to an Integer

---
+++ 008_Boolean_int_conversion/README.md
@@ -0,0 +1,12 @@
+Exercise 8: Convert a Boolean to an Integer
+
+## Objective 
+Learn how Boolean values convert to integers.
+
+## Task
+   * Convert the Boolean values True and False into integers.
+   * Print their numerical values.
+
+## Expected Output
+- True as an integer: 1
+- False as an integer: 0
---
+++ 008_Boolean_int_conversion/main.py
@@ -0,0 +1,11 @@
+# Exercise 8: Convert a Boolean to an Integer
+
+# --- Part 1: Convert the Boolean value True into integer ---
+bool_true = True
+int_from_true = int(bool_true)
+print(f"{bool_true} as an integer: {int_from_true}")
+
+# --- Part 2: Convert the Boolean value False into integer ---
+bool_false = False
+int_from_false = int(bool_false)
+print(f"{bool_false} as an integer: {int_from_false}")
