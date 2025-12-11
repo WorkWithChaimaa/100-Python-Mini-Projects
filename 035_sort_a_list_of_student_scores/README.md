00ec23d2f6db2070bffc5ba02b8f5f1deac7dbdf
chaimaa <chamitachomy29@gmail.com>
2025-12-11 14:36:23 -0800

    Exercise 35: Sort a List of Student Scores

---
+++ 035_sort_a_list_of_student_scores/README.md
@@ -0,0 +1,10 @@
+Exercise 35: Sort a List of Student Scores
+
+## Objective: 
+Learn how to sort a list.
+
+## Task:
+    • Given the list of scores [88, 92, 75, 100, 89], sort them in ascending order.
+
+## Expected Output:
+    [75, 88, 89, 92, 100]
---
+++ 035_sort_a_list_of_student_scores/main.py
@@ -0,0 +1,11 @@
+# Exercise 35: Sort a List of Student Scores
+
+# 1. Initialize the list of scores
+student_scores = [88, 92, 75, 100, 89]
+
+# 2. Sort the list IN PLACE using the .sort() method.
+# By default, .sort() sorts in ascending order.
+student_scores.sort()
+
+# 3. Print the sorted list
+print(student_scores)
