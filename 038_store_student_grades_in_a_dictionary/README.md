4ba4e7a992454c07c7b66325786a322c1c53ed8e
chaimaa <chamitachomy29@gmail.com>
2025-12-11 15:07:33 -0800

    Exercise 38: Store Student Grades in a Dictionary

---
+++ 038_store_student_grades_in_a_dictionary/README.md
@@ -0,0 +1,11 @@
+Exercise 38: Store Student Grades in a Dictionary
+
+## Objective: 
+Store and retrieve student grades using a dictionary.
+
+## Task:
+    • Create a dictionary where student names are keys and their grades are values.
+    • Retrieve the grade of ”Sarah”.
+   
+## Expected Output:
+    Sarah's grade: A
---
+++ 038_store_student_grades_in_a_dictionary/main.py
@@ -0,0 +1,13 @@
+# Exercise 38: Store Student Grades in a Dictionary
+
+# 1. Create the dictionary with student names (keys) and grades (values)
+student_grades = {
+    "James": "A",
+    "Sarah": "A",
+    "carolina": "B",
+    "John": "B"
+}
+
+# 2. Retrieve the grade of "Sarah" and print the result
+# The square bracket notation [key] retrieves the value.
+print(f"Sarah's grade: {student_grades['Sarah']}")
