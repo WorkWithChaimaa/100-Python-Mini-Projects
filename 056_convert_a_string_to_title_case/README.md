e28c929f9f6b9914b1ae7a8477ab830d2f0933d2
chaimaa <chamitachomy29@gmail.com>
2025-12-14 04:28:33 -0800

    Exercise 56: Convert a String to Title Case

---
+++ 056_convert_a_string_to_title_case/README.md
@@ -0,0 +1,10 @@
+Exercise 56: Convert a String to Title Case
+
+## Objective: 
+    Convert a string so that each word starts with a capital letter.
+
+## Task:
+    • Convert ”lkhibra academy python training” to title case.
+
+## Expected Output:
+    Lkhibra Academy Python Training
---
+++ 056_convert_a_string_to_title_case/main.py
@@ -0,0 +1,10 @@
+# Exercise 56: Convert a String to Title Case
+
+# The input string
+input_string = "lkhibra academy python training"
+
+# Apply the .title() method to convert the string to title case
+title_cased_string = input_string.title()
+
+# Print the result
+print(title_cased_string)
