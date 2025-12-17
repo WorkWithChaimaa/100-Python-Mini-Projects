fcd663688e044df4a6ea10a4dbdd2f43591fffda
chaimaa <chamitachomy29@gmail.com>
2025-12-16 20:28:25 -0800

    Exercise 66: Student Grade Management System

---
+++ 066_student_grade_management_system/README.md
@@ -0,0 +1,20 @@
+Project 66: Student Grade Management System
+
+## Objective:
+    Build a system to store, update, and retrieve student grades.
+
+## Task:
+    • Create a dictionary where student names are keys and their grades are values.
+    • Allow users to: Add a new student. Update an existing grade. Retrieve a student’s
+    grade. Display all students.
+
+## Expected Output:
+1. Add Student
+2. Update Grade
+3. Get Grade
+4. Display All
+5. Exit
+Enter choice: 1
+Enter student name: Alice
+Enter grade: A
+Student added successfully.
---
+++ 066_student_grade_management_system/main.py
@@ -0,0 +1,45 @@
+# Exercise 66: Student Grade Management System
+
+students = {}
+def add_students():
+    name = input("Enter student name: ")
+    grade = input("Enter grade: ")
+    students[name] = grade
+    print("Student added successfully")
+
+def update_grade():
+    name = input("Enter student name: ")
+    if name in students:
+        grade = input("Enter grade: ")
+        students[name] = grade
+        print("Grade updated successfully")
+    else:
+        print("Student not found")
+
+def get_grade():
+    name = input("Enter student name: ")
+    if name in students:
+        print(students[name])
+    else:
+        print("Student not found")
+
+def display_all():
+    for key, value in students.items():
+        print(f"{key}: {value}")
+while True:
+    print("\n1. Add students\n2. Update grade\n3. Get grade\n4. Display all\n5. Exit")
+    choice = input("Enter you choice: ")
+    if choice == "1":
+        add_students()
+    elif choice == "2":
+        update_grade()
+    elif choice == "3":
+        get_grade()
+    elif choice == "4":
+        display_all()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice")
+
+print(students)
